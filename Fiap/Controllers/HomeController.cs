7d04f27aa0a1349f0781f3bde168d880cc15b446
titoluco <titoluco@gmail.com>
2019-09-11 18:58:20 -0300

    Montagem sln

---
+++ Fiap/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fiap.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            ViewData["NomeAluno"] = "Thiago";
+            ViewBag.NomeAluno = "Josefo";
+
+            var pessoa = new Pessoa() { Nome = "Astolfo" };
+            var homeViewModel = new HomeViewModel(pessoa);
+
+            return View(pessoa);
+        }
+        [HttpPost]
+        public ActionResult Index([FromBody]Pessoa pessoa, [FromHeader]DadosX dadosX)
+        {
+            return View();
+        }
+        public ActionResult Edit(int nomeDoCliente)
+        {
+            return View();
+        }
+        public ActionResult Salvar(Pessoa pessoa)
+        {
+            return View();
+        }
+
+        public ActionResult Sobre()
+        {
+            return View();
+        }
+    }
+
+    internal class HomeViewModel
+    {
+        public Pessoa pessoa { get; set; }
+        public HomeViewModel(Pessoa pessoa)
+        {
+            this.pessoa = pessoa;
+        }
+    }
+
+    public class Pessoa
+    {
+        public string Nome { get; set; }
+    }
+
+    public class DadosX
+    {
+        public string AppVersion { get; set; }
+    }
+}
---
+++ Fiap/Program.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace Fiap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args)
+        {
+            return WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .Build();
+        }
+    }
+
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection service)
+        {
+            service.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            //app.Run(async (context) =>
+            //{
+            //    await context.Response.WriteAsync("boa noite nada!!!");
+            //});
+
+            app.UseStaticFiles();
+
+            app.UseMvc(
+                routes =>
+                {
+                    routes.MapRoute(
+                        name: "padrao, default",
+                        template: "{controller=Home}/{action=Index}/{id?}"
+                        );
+                });
+        }
+    }
+}
